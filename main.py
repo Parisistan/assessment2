f88491d17dc7e7d9b8db1c4fc6de7e86aec82731
Parisistan <71983551+Parisistan@users.noreply.github.com>
2022-04-07 09:23:18 +0100

    running fuction

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ def two_number_sum(numbers, target):
     return []
 
 
-# numbers = [4, 17, 3, -10, 0, 6, 20, 11]
-# target = 10
-# print(two_number_sum(numbers, target))
+numbers = [4, 17, 3, -10, 0, 6, 20, 11]
+target = 10
+print(two_number_sum(numbers, target))
+
 
 # version 2
 # try:
-#     f = open(file.txt)
+#     
 # not all numbers
 # except Exception:
-#     print("this file is nowhere to be found!")
+#     
 # else:
 #
 # finally:
